d61e3aa16439aefae2811268646a7f3795624437
macostaa <113563773+macostaa@users.noreply.github.com>
2022-10-30 19:00:19 -0400

    B4_e09_Ciclos
    Todos los números del 10 al 0 de 7 en 7

---
+++ B4_e09_Ciclos/src/b4_e09_ciclos/B4_e09_Ciclos.java
@@ -0,0 +1,21 @@
+/*
+Todos los números del 10 al 0 de 7 en 7
+ */
+package b4_e09_ciclos;
+
+/**
+ *
+ * @author home
+ */
+public class B4_e09_Ciclos {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for(int i=100;i>=0;i-=7){
+            System.out.print(i+" ");
+        }
+    }
+    
+}
